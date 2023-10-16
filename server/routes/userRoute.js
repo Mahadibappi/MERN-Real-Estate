de9be10e57e9b332407c1fac28d23c0b6e7b0ec4
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-10-16 19:03:25 +0600

    server created

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser, getUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/signUp", createUser);
-
-router.get("/login", getUser);
-export default router;
