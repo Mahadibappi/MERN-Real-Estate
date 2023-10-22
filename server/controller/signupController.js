2b82658d9c61e5efd0cea75c4eb555403650163a
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-10-22 23:08:21 +0600

    login function created

--- server/controller/signupController.js
+++ server/controller/signupController.js
@@ -1,12 +1,13 @@
 import User from "../models/userModel.js";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/Error.js";
 
 //create new user
 export const singUp = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
+    const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json("user created successfully");
@@ -17,13 +18,14 @@ export const singUp = async (req, res, next) => {
 
 // login function
 export const login = async (req, res, next) => {
+  const { email, password } = req.body;
   try {
-    const { email, password } = req.body;
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
-    const validPassword = bcrypt.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong password"));
-    res.status(200).json("found this user");
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+    const { password: pass, ...rest } = validUser._doc;
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
