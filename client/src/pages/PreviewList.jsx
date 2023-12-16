4b29e15a9517585b85d98b8be2632624c1bc01ed
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-12-16 23:47:51 +0600

    preview list done

--- client/src/pages/PreviewList.jsx
+++ client/src/pages/PreviewList.jsx
@@ -4,8 +4,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
-import { FaBed } from "react-icons/fa";
+import { FaBed, FaChair, FaParking } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaBath } from "react-icons/fa6";
+import { MdLocalParking } from "react-icons/md";
+import { MdChair } from "react-icons/md";
 const PreviewList = () => {
   SwiperCore.use([Navigation]);
   const params = useParams();
@@ -60,11 +63,11 @@ const PreviewList = () => {
           </Swiper>
         </div>
       )}
-      <div className="flex flex-col max-w-4xl text-center mx-auto my-5">
+      <div className="flex flex-col justify-center items-center max-w-4xl mx-auto p-3 my-5 gap-2">
         {listing?.map((list) => (
           // eslint-disable-next-line react/jsx-key
           <>
-            <p className="text-2xl text-white">
+            <p className="text-2xl text-white lg:ml-[-165px]">
               {list.name} - ${""}
               {list.offer
                 ? list.discountPrice.toLocaleString("en-US")
@@ -72,21 +75,46 @@ const PreviewList = () => {
               {listing.type === "rent" && " / month"}
             </p>
 
-            <p className=" flex items-center mx-auto ml-2  gap-2 text-xl text-white p-2 ">
-              <FaMapMarkerAlt className=" text-white" />
+            <p className=" flex items-center sm:ml-[110px] md:ml-[115px] mx-auto  gap-2 text-md text-white p-2 ">
+              <FaMapMarkerAlt className="text-white" />
               {list.address}
             </p>
-            <div className=" flex items-center gap-4 mx-auto sm:ml-40 lg:ml-60">
-              <p className="text-sm bg-red-600 text-white px-3 rounded-md">
+            <div className=" flex items-center gap-4 mx-auto sm:ml-[125px] md:ml-[115px]">
+              <p className="text-sm bg-red-600 text-white px-3 py-1 rounded-md">
                 {list.type === "rent" ? "For Rent" : "For Sale"}
               </p>
               {list.offer && (
-                <p className="text-sm bg-green-600 text-white px-3 rounded-md">
-                  {" "}
+                <p className="text-sm bg-green-600 text-white px-3 py-1  rounded-md">
                   Discount ${+list.regularPrice - +list.discountPrice} Off
                 </p>
               )}
             </div>
+            <p className=" text-sm sm:ml-[115px] lg:ml-[115px] text-slate-200">
+              <span className="font-semibold">Description - </span>
+              {list.description}
+            </p>
+            <ul className=" flex items-center mt-2 gap-4 text-green-700 sm:ml-[-40px] md:ml-[-235px] ">
+              <li className="flex  items-center gap-2 text-md ">
+                <FaBed className="text-3xl" />
+                {list.bedrooms > 1
+                  ? `${list.bedrooms} beds`
+                  : `${list.bedrooms} bed`}
+              </li>
+              <li className="flex items-center gap-2">
+                <FaBath className="text-2xl" />
+                {list.bathrooms > 1
+                  ? `${list.bathrooms} baths`
+                  : `${list.bathrooms} bath`}
+              </li>
+              <li className="flex items-center gap-2">
+                <FaParking className="text-2xl" />
+                {list.parking ? "parking" : "no parking"}
+              </li>
+              <li className="flex  items-center gap-2">
+                <FaChair className="text-2xl" />
+                {list.furnished ? "furnished" : "not furnished"}
+              </li>
+            </ul>
           </>
         ))}
       </div>
